81f1022e97538dc3a3ff17c7f7facd790cb44027
Roman <roman.klemiato18@gmail.com>
2025-04-16 16:09:50 +0300

    responsive design stuff

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,30 +2,47 @@ import React from "react";
 import HeroImg from "../assets/images/desktop/image-hero.jpg";
 import IconRight from "../assets/icons/icon-arrow-right.svg";
 
+import HeroImgDesktop from "../assets/images/desktop/image-hero.jpg";
+import HeroImgTablet from "../assets/images/tablet/image-hero.jpg";
+import HeroImgMobile from "../assets/images/mobile/image-hero.jpg";
+
 import GridImageOneDesktop from "../assets/images/desktop/image-grid-1.jpg";
 import GridImageOneTablet from "../assets/images/tablet/image-grid-1.jpg";
 import GridImageOneMobile from "../assets/images/tablet/image-grid-1.jpg";
 
+import GridImageTwoDesktop from "../assets/images/desktop/image-grid-2.jpg";
+import GridImageTwoTablet from "../assets/images/tablet/image-grid-2.jpg";
+import GridImageTwoMobile from "../assets/images/tablet/image-grid-2.jpg";
 
+import GridImageThreeDesktop from "../assets/images/desktop/image-grid-3.jpg";
+import GridImageThreeTablet from "../assets/images/tablet/image-grid-3.jpg";
+import GridImageThreeMobile from "../assets/images/mobile/image-grid-3.jpg";
 
 import GridImageTwo from "../assets/images/desktop/image-grid-2.jpg";
 import GridImageThree from "../assets/images/desktop/image-grid-3.jpg";
 import FacebookIcon from "../assets/icons/icon-facebook.svg";
 import TwitterIcon from "../assets/icons/icon-twitter.svg";
 import InstagramIcon from "../assets/icons/icon-instagram.svg";
+import { Link } from "react-router";
 
 const HomePage = () => {
   return (
     <div>
-      <header className="grid grid-cols-[5fr_3fr]">
-        <div className="">
-          <div className="bg-black hidden lg:block">here</div>
-          <figure>
-            <img src={HeroImg} alt="hero-image" />
-          </figure>
+      <header className="grid grid-cols-1 md:grid-cols-[5fr_3fr] h-[800px] max-h-[800px]">
+        <div className="h-64 min-h-64 w-full md:h-full">
+          
+          <picture className="w-full h-64 sm:h-80 ">
+            <source media="(min-width: 1024px)" srcSet={HeroImgDesktop} />
+            <source media="(min-width: 640px)" srcSet={HeroImgTablet} />
+            <img
+              src={HeroImgMobile}
+              alt="hero-image"
+              className="w-full h-full object-cover md:object-contain"
+            />
+          </picture>
         </div>
-        <div className="flex flex-col justify-center -ml-30">
-          <h1 className="font-big text-[96px] font-extrabold leading-23">
+        <div className="flex flex-col justify-center md:-ml-30 space-y-8  py-10 px-5 ">
+          <h1 className="font-big text-7xl md:text-[96px] font-extrabold leading-16 md:leading-23">
             MODERN <br /> ART GALLERY
           </h1>
           <p className="w-96">
@@ -33,18 +50,20 @@ const HomePage = () => {
             from a spark of inspiration. Will these pieces inspire you? Visit us
             and find out.
           </p>
-          <button className="flex">
-            <span className="bg-[#151515] py-5 px-8 text-white font-big text-2xl tracking-widest">OUR LOCATION</span>
-            <div className="bg-[#D5966C] px-4 flex items-center ">
+          <Link className="flex group" to="/location">
+            <span className="bg-[#151515] py-5 px-8 text-white font-big text-2xl tracking-widest group-hover:bg-[#D5966C] transition-colors duration-300">
+              OUR LOCATION
+            </span>
+            <div className="bg-[#D5966C] px-4 flex items-center group-hover:bg-[#151515] transition-colors duration-300"> 
               <img src={IconRight} alt="Right icon" className="w-6 h-6" />
             </div>
-          </button>
+          </Link>
         </div>
       </header>
-      <main className="mt-40">
-        <div className="grid grid-cols-[1fr_2fr] px-10 lg:px-40 gap-x-32">
-          <div className="flex flex-col items-center justify-center">
-            <div className="self-start w-[270px]">
+      <main className="mt-26 md:mt-40">
+        <div className="grid grid-cols-1  md:grid-cols-[1fr_2fr] px-5 md:px-10 lg:px-40 gap-x-32">
+          <div className="flex flex-col items-center justify-center order-1 md:order-0 mt-5">
+            <div className="md:self-start md:w-[270px]">
               <div className="mb-5">
                 <h1 className="font-big text-[60px] font-bold leading-16 tracking-tight">
                   YOUR DAY AT
@@ -53,42 +72,59 @@ const HomePage = () => {
                   THE GALLERY
                 </h1>
               </div>
-              <p className="">
+              <p className="text-lg">
                 Wander through our distinct collections and find new insights
                 about our artists. Dive into the details of their creative
                 process
               </p>
             </div>
           </div>
-          <div className="flex justify-end">
-            <picture>
-              <source media="(min-width: 1024px)" srcSet={GridImageOneDesktop} />
+          <div className="flex md:justify-end">
+            <picture className="w-full h-full">
+              <source
+                media="(min-width: 1024px)"
+                srcSet={GridImageOneDesktop}
+              />
               <source media="(min-width: 640px)" srcSet={GridImageOneTablet} />
               <img
                 src={GridImageOneMobile}
                 alt="hero-image"
-                className="w-full h-auto"
+                className="w-full h-full object-cover"
               />
             </picture>
           </div>
         </div>
-        <div className="grid grid-cols-[6fr_4fr] px-10 lg:px-40 mt-8 pb-20 gap-x-5">
-          <figure className="h-full">
-            <img src={GridImageTwo} alt="grid-image-two"  className="w-full h-full"/>
-          </figure>
-          <div className=" flex flex-col gap-6">
-            <figure className="h-full ">
-              <img
-                src={GridImageThree}
-                alt="grid-image-three"
-                className="h-full"
+        <div className="grid grid-cols-1 gap-y-6 md:grid-cols-[6fr_4fr] px-5 md:px-10 lg:px-40 mt-8 pb-20 gap-x-5">
+          <picture className="w-full h-[480px] md:h-full">
+            <source media="(min-width: 1024px)" srcSet={GridImageTwoDesktop} />
+            <source media="(min-width: 640px)" srcSet={GridImageTwoTablet} />
+            <img
+              src={GridImageTwoMobile}
+              alt="grid-image-two"
+              className="w-full h-full"
+            />
+          </picture>
+          <div className=" flex flex-col gap-6 h-full">
+            <picture className="w-full h-64 flex-1">
+              <source
+                media="(min-width: 1024px)"
+                srcSet={GridImageThreeDesktop}
               />
-            </figure>
-            <div className="bg-[#151515] w-full h-full flex flex-col items-center justify-center p-10 gap-8 ">
-              <h1 className="font-big text-[60px] text-white font-bold leading-16 self-start">
+              <source
+                media="(min-width: 640px)"
+                srcSet={GridImageThreeTablet}
+              />
+              <img
+                src={GridImageThreeMobile}
+                alt="grid-image-three"
+                className="w-full h-full object-cover"
+              />
+            </picture>
+            <div className="bg-[#151515] w-full h-full flex flex-col items-center justify-center p-10 gap-2 lg:gap-8 flex-1">
+              <h1 className="font-big  text-[50px] lg:text-[60px] text-white font-bold leading-16 self-start">
                 COME & BE <br /> INSPIRED
               </h1>
-              <p className="text-white text-xl">
+              <p className="text-white text-md lg:text-xl">
                 We're excited to welcome you to our gallery and see how our
                 collections influence you.
               </p>
@@ -96,16 +132,16 @@ const HomePage = () => {
           </div>
         </div>
       </main>
-      <footer className="bg-[#151515] py-20">
-        <div className="flex items-center justify-between px-40">
-          <h2 className="font-big text-[34px] leading-9 text-white">
+      <footer className="bg-[#151515] py-20 mt-20 md:mt-0">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center  gap-5 px-10 lg:px-40">
+          <h2 className="font-big text-3xl font-bold lg:text-[34px] leading-9 text-white">
             MODERN <br /> ART GALLERY
           </h2>
-          <p className="text-white w-96 -mb-5 mr-8">
+          <p className="text-white text-lg w-96 md:-mb-5 mr-8">
             The Modern Art Gallery is free to all visitors and open seven days a
             week from 8am to 9pm. Find us at 99 King Street, Newport, USA
           </p>
-          <div className="flex gap-3 -mt-12">
+          <div className="flex gap-3 md:-mt-12">
             <span>
               <img src={FacebookIcon} alt="facebook-icon" />
             </span>
--- src/pages/LocationPage.jsx
+++ src/pages/LocationPage.jsx
@@ -4,21 +4,40 @@ import FacebookIcon from "../assets/icons/icon-facebook.svg";
 import TwitterIcon from "../assets/icons/icon-twitter.svg";
 import InstagramIcon from "../assets/icons/icon-instagram.svg";
 
+import MapImgDesktop from "../assets/images/desktop/image-map.png";
+import MapImgTablet from "../assets/images/tablet/image-map.png";
+import MapImgMobile from "../assets/images/mobile/image-map.png";
+
+import IconArrowLeft from "../assets/icons/icon-arrow-left.svg";
+import { Link } from "react-router";
 
 const LocationPage = () => {
   return (
     <>
-      <section className=" relative">
-        <figure>
-          <img src={MapImg} alt="map-image" />
-        </figure>
-        <button className="absolute top-0 left-40">Back To home</button>
+      <section className=" relative h-[550px] md:h-auto">
+        <picture className="w-full h-full">
+          <source media="(min-width: 1024px)" srcSet={MapImgDesktop} />
+          <source media="(min-width: 640px)" srcSet={MapImgTablet} />
+          <img
+            src={MapImgMobile}
+            alt="map-image"
+            className="w-full h-full object-cover"
+          />
+        </picture>
+        <Link className="group flex absolute top-0 md:left-40" to="/">
+          <div className="bg-[#D5966C] px-4 flex items-center group-hover:bg-[#151515] transition-colors duration-300">
+            <img src={IconArrowLeft} alt="Right icon" className="w-6 h-6" />
+          </div>
+          <span className="bg-[#151515] py-5 px-8 text-white font-big text-2xl tracking-widest group-hover:bg-[#D5966C] transition-colors duration-300">
+            Back Home
+          </span>
+        </Link>
       </section>
-      <section className="bg-[#151515] grid grid-cols-2 px-40 py-20">
-        <h1 className="text-7xl text-white font-big font-bold">
-          OUR <br /> LOCATION
+      <section className="bg-[#151515] grid grid-cols-1 md:grid-cols-2 px-5 md:px-10 lg:px-40 py-20">
+        <h1 className="text-6xl md:text-7xl text-white font-big font-bold">
+          OUR <br className="hidden md:block" /> LOCATION
         </h1>
-        <div>
+        <div className="mt-10 md:mt-0">
           <h3 className="font-big font-bold text-4xl text-orange-200 mb-6">
             99 KING STREET
           </h3>
@@ -27,7 +46,7 @@ const LocationPage = () => {
             RI 02840 <br />
             United States of America
           </p>
-          <p className="text-white w-[420px] text-lg">
+          <p className="text-white md:w-[380px] lg:w-[420px] text-lg">
             Our newly opened gallery is located near the Edward King House on 99
             King Street, the Modern Art Gallery is free to all visitors and open
             seven days a week from 8am to 9pm.
@@ -35,15 +54,15 @@ const LocationPage = () => {
         </div>
       </section>
       <footer className="bg-[#d5966c] py-20">
-        <div className="flex items-center justify-between px-40">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center  gap-5 px-10 lg:px-40">
           <h2 className="font-big text-[34px] leading-9 text-black font-bold">
             MODERN <br /> ART GALLERY
           </h2>
-          <p className="text-gray-800 w-96 -mb-5 mr-8">
+          <p className="text-gray-800 w-96 md:-mb-5 mr-8">
             The Modern Art Gallery is free to all visitors and open seven days a
             week from 8am to 9pm. Find us at 99 King Street, Newport, USA
           </p>
-          <div className="flex gap-3 -mt-12">
+          <div className="flex gap-3 md:-mt-12">
             <span>
               <img src={FacebookIcon} alt="facebook-icon" />
             </span>
